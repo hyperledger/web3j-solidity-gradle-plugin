da0c17a66b8d99bb7a692a3f11185f1756a5ea60
Xavier Arias <xavier@blk.io>
2018-08-29 18:51:21 +0100

    Move action classes into plugin methods for simplicity

--- src/main/groovy/org/web3j/solidity/gradle/plugin/SolidityPlugin.groovy
+++ src/main/groovy/org/web3j/solidity/gradle/plugin/SolidityPlugin.groovy
@@ -5,9 +5,13 @@ import org.gradle.api.Project
 import org.gradle.api.internal.file.SourceDirectorySetFactory
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
 
 import javax.inject.Inject
+
+import static org.codehaus.groovy.runtime.StringGroovyMethods.capitalize
+import static org.web3j.solidity.gradle.plugin.SoliditySourceSet.NAME
 
 /**
  * Gradle plugin for Solidity compile automation.
@@ -30,16 +34,62 @@ class SolidityPlugin implements Plugin<Project> {
         final SourceSetContainer sourceSets = target.convention
                 .getPlugin(JavaPluginConvention.class).sourceSets
 
-        final SoliditySourceSetAction sourceSetAction =
-                new SoliditySourceSetAction(target, sourceFactory)
-
-        final ConfigureTasks compileSolidity = new ConfigureTasks(target)
-
-        sourceSets.all(sourceSetAction)
+        sourceSets.all { configureSourceSet(target, it) }
 
         target.afterEvaluate {
-            sourceSets.all(compileSolidity)
+            sourceSets.all { configureTask(target, it) }
         }
+    }
+
+    /**
+     * Add default source set for Solidity.
+     */
+    private void configureSourceSet(final Project project, final SourceSet sourceSet) {
+
+        def srcSetName = capitalize((CharSequence) sourceSet.name)
+        def soliditySourceSet = new DefaultSoliditySourceSet(srcSetName, sourceFactory)
+
+        sourceSet.convention.plugins.put(NAME, soliditySourceSet)
+
+        def defaultSrcDir = new File(project.projectDir, "src/$sourceSet.name/$NAME")
+        def defaultOutputDir = new File(project.buildDir, "resources/$sourceSet.name/$NAME")
+
+        soliditySourceSet.solidity.srcDir(defaultSrcDir)
+        soliditySourceSet.solidity.outputDir = defaultOutputDir
+
+        sourceSet.allJava.source(soliditySourceSet.solidity)
+        sourceSet.allSource.source(soliditySourceSet.solidity)
+    }
+
+    /**
+     * Configures code compilation tasks for the Solidity source sets defined in the project
+     * (e.g. main, test).
+     * <p>
+     * By default the generated task name for the <code>main</code> source set
+     * is <code>compileSolidity</code> and for <code>test</code>
+     * <code>compileTestSolidity</code>.
+     */
+    private void configureTask(final Project project, final SourceSet sourceSet) {
+
+        def srcSetName = sourceSet.name == 'main' ?
+                '' : capitalize((CharSequence) sourceSet.name)
+
+        def compileTask = project.tasks.create(
+                "compile" + srcSetName + "Solidity", CompileSolidity, sourceSet)
+
+        def soliditySourceSet = sourceSet.convention.plugins[NAME] as SoliditySourceSet
+
+        // Set the sources for the generation task
+        compileTask.setSource(soliditySourceSet.solidity)
+
+        compileTask.outputComponents = project.solidity.outputComponents
+        compileTask.overwrite = project.solidity.overwrite
+        compileTask.optimize = project.solidity.optimize
+        compileTask.optimizeRuns = project.solidity.optimizeRuns
+        compileTask.prettyJson = project.solidity.prettyJson
+        compileTask.outputs.dir(soliditySourceSet.solidity.outputDir)
+        compileTask.description = "Generates web3j contract wrappers " +
+                "for $sourceSet.name source set."
     }
 
 }
