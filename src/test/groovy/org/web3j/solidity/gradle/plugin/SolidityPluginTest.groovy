c8301b1a35a74382239ef5b9295a8e4ddddb232d
gtebrean <99179176+gtebrean@users.noreply.github.com>
2023-06-14 18:21:52 +0300

    Update SolidityPluginTest.groovy

--- src/test/groovy/org/web3j/solidity/gradle/plugin/SolidityPluginTest.groovy
+++ src/test/groovy/org/web3j/solidity/gradle/plugin/SolidityPluginTest.groovy
@@ -329,7 +329,6 @@ class SolidityPluginTest {
                 .withPluginClasspath()
                 .forwardOutput()
                 .withDebug(true)
-        .
                 .build()
     }
 }
